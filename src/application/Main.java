e51e5a283f6ffaeed87f72ccb0a3ae9483aa6aaa
Lourenço Basso <l.oliveirabasso@gmail.com>
2020-08-04 11:19:36 -0300

    Adicionados métodos sacar e depositar

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,15 @@ class Conta {
 	int numero;
 	String titular;
 	double saldo;
+	
+	void sacar(double quantidade) {
+		double novoSaldo = this.saldo - quantidade;
+		this.saldo = novoSaldo;
+	}
+	
+	void depositar(double quantidade) {
+		this.saldo += quantidade;
+	}
 }
 
 public class Main {
@@ -13,6 +22,8 @@ public class Main {
 		
 		c1.titular = "Fulano de Tal";
 		c1.saldo = 1000.0;
+		c1.sacar(500.0);
+		c1.depositar(200.0);
 		
 		System.out.println("Saldo da conta de " + c1.titular + ": R$ " + c1.saldo);
 	}
